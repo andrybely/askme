9e229abdb69192da295c85bf0f0f48348db55407
bely <bely10@ya.ru>
2016-10-09 17:13:01 +0300

    add questions count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,26 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonenie(number)
+    if (number == nil || !number.is_a?(Numeric))
+      number = 0
+    end
+
+    ostatok100 = number % 100
+
+    if (ostatok100 >= 11 && ostatok100 <= 14)
+      return " вопросов"
+    end
+    ostatok = number % 10
+    if (ostatok == 1)
+      return " вопрос"
+    end
+    if (ostatok >= 2 && ostatok <= 4)
+      return " вопроса"
+    end
+    if (ostatok > 4 || ostatok == 0)
+      return " вопросов"
+    end
+  end
 end
