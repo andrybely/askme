9f4a8952d6484e66f351b865ace2950e3447f71e
bely <bely10@ya.ru>
2016-10-14 20:32:29 +0300

    users+sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :create, :new]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
 
     if @user.save
@@ -20,10 +27,30 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные успешно обновлены!'
+    else
+      render 'edit'
+    end
+  end
+
   def show
+    @questions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
                                  :name, :username, :avatar_url)
